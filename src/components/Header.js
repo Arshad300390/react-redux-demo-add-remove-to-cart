e198dd8310e8ab1beec6db7f2cc5539d2a0b8a81
arshad ali <arshad300390@gmail.com>
2023-09-09 21:45:57 +0500

    complete after some changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 function Header(props) {
-    console.warn('Header', props)
+    console.warn('Header', props.HeadercartData.cartData.length)
     return (
         <div>
             <div className="add-to-cart">
-                <span className="cart-count">{ props.HeadercartData}</span>
+                <span className="cart-count">{ props.HeadercartData.cartData.length}</span>
                 <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" alt='cart-img'/>
+                <span className='cart-count' id='cart-count'>{ props.HeadercartData.totalPrice}</span>
             </div>
         </div>
     )
--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -2,7 +2,7 @@ import {connect} from 'react-redux'
 import Header from '../components/Header'
 
 const mapStateToProps = state => ({
-    HeadercartData:state.cartItems.cartData.length
+    HeadercartData:state.cartItems,
 })
 
 const mapDispatchToProps = dispatch => ({
